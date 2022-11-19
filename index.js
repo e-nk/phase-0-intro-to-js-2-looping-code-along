d45954cdf132b1cabdffbadb41f31976b243fd7a
e-nk <enocklagat747@gmail.com>
2022-11-19 14:17:13 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = ["surprise"];
+function writeCards(names, event){
+  let greeting = []
+  for (let i = 0; i < names.length; i++){
+     greeting.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return greeting;
+    
+
+}
+
+function countDown(num){
+  while (num >= 0){
+    console.log(num--);
+      }
+
+}
